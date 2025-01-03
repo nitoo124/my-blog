99d46f3d859c605296bf5e1176215320c8bec7cf
Nitoo kumari <160069703+nitoo124@users.noreply.github.com>
2025-01-03 06:37:45 -0800

    main

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  estlint:{
+    ignoreDuringBuilds:true,
+
+  },
+
   /* config options here */
   images: {remotePatterns:[
     {
